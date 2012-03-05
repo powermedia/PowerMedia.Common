d03eff5dc99f494c509e8290728030ef262a0f3d
Cyryl Plotnicki-Chudyk <cchudyk@power.com.pl>
2012-03-05 12:29:36 +0100

    FilesController base class for serving aplication-wide downloads added ; Pagination helper added ; HttpRequestBase extensions

---
+++ sources/PowerMedia.Common/Data/Pagination.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PowerMedia.Common.Data
+{
+    [Serializable]
+    public class PaginationSettings
+    {
+        public uint? ItemsPerPageLimit;
+        public uint? CurrentPageNumber;
+    }
+
+    public class Paginator<T>
+    {
+        private static readonly uint DEFAULT_ITEMS_PER_PAGE_LIMIT = 20;
+
+        private IEnumerable<T> _collection;
+        private PaginationSettings _settings;
+        public Paginator(PaginationSettings settings, IEnumerable<T> collection)
+        {
+            if (collection == null) { throw new ArgumentNullException(); }
+            if (settings == null)
+            {
+                settings = new PaginationSettings();
+            }
+            _settings = settings;
+            _collection = collection;
+        }
+
+        private uint ItemsPerPageLimit
+        {
+            get
+            {
+                if (_settings.ItemsPerPageLimit == null)
+                {
+                    return DEFAULT_ITEMS_PER_PAGE_LIMIT;
+                }
+                else
+                {
+                    return _settings.ItemsPerPageLimit.Value;
+                }
+            }
+        }
+
+        uint? _totalItemsCount;
+        private uint TotalItemsCount
+        {
+            get
+            {
+                lock (this)
+                {
+                    if (_totalItemsCount == null)
+                    {
+                        _totalItemsCount = (uint)_collection.Count();
+                    }
+                }
+                return _totalItemsCount.Value;
+            }
+        }
+
+        private uint? _totalPageCount;
+        public uint TotalPageCount
+        {
+            get
+            {
+                lock (this)
+                {
+                    if (_totalPageCount == null)
+                    {
+                        _totalPageCount = (uint)Math.Ceiling((double)TotalItemsCount / (double)ItemsPerPageLimit);
+                        _totalPageCount = Math.Min(_totalPageCount.Value, 30);
+                    }
+                }
+                return _totalPageCount.Value;
+            }
+        }
+
+        /// <summary>
+        /// always >= 1
+        /// </summary>
+        public uint CurrentPageNumber
+        {
+            get
+            {
+                uint pageNumberCandidate = 1;
+                if (_settings.CurrentPageNumber.HasValue) { pageNumberCandidate = _settings.CurrentPageNumber.Value; }
+
+                //normalize in case some items gone missing between requests
+                pageNumberCandidate = Math.Min(pageNumberCandidate, TotalPageCount);
+                return pageNumberCandidate;
+            }
+        }
+
+        private uint FirstItemOnCurrentPageNumber
+        {
+            get
+            {
+                return SkipItemsNumber + 1;
+            }
+        }
+
+        private uint SkipItemsNumber
+        {
+            get
+            {
+                return ItemsPerPageLimit * (CurrentPageNumber - 1);
+            }
+        }
+
+        private uint TakeItemsNumber
+        {
+            get
+            {
+                return ItemsPerPageLimit;
+            }
+        }
+
+        IEnumerable<T> _itemsOnCurrentPage;
+        public IEnumerable<T> ItemsOnCurrentPage
+        {
+            get
+            {
+                if (_itemsOnCurrentPage != null)
+                {
+                    return _itemsOnCurrentPage;
+                }
+                lock (this)
+                {
+                    if (_itemsOnCurrentPage == null)
+                    {
+                        if (ItemsPerPageLimit == 0)
+                        {
+                            _itemsOnCurrentPage = _collection;
+                        }
+                        _itemsOnCurrentPage = _collection.Skip((int)SkipItemsNumber).Take((int)TakeItemsNumber);
+                        _itemsOnCurrentPage = _itemsOnCurrentPage.ToList();
+                    }
+                }
+                return _itemsOnCurrentPage;
+            }
+        }
+
+    }
+}
---
+++ sources/PowerMedia.Common/System/Web/RequestExtensions.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Collections.Specialized;
+using System.Web.Routing;
+
+namespace PowerMedia.Common.System.Web
+{
+    public static class RequestExtensions
+    {
+        public static ForwardableRoute ForwardRoute(this HttpRequestBase request)
+        {
+            return new ForwardableRoute(getRouteValues(request.QueryString));
+        }
+
+        public static bool GetBooleanParameterValue(this HttpRequestBase request, string name, bool defaultValue = false)
+        {
+            var result = DetermineBooleanParameterValue(request.QueryString, name);
+            if (result.HasValue)
+            {
+                return result.Value;
+            }
+            return defaultValue;
+        }
+
+        private static bool? DetermineBooleanParameterValue(this HttpRequestBase request, string name)
+        {
+            return DetermineBooleanParameterValue(request.QueryString, name);
+        }
+
+        private static bool? DetermineBooleanParameterValue(NameValueCollection parameters, string name)
+        {
+            var parameter = parameters[name];
+            if (parameter == null) { return null; }
+            var values = parameter.Split(',');
+            bool result = false;
+            var parsedAnyBool = false;
+            foreach (var value in values)
+            {
+                bool parsedValue;
+                parsedAnyBool |= Boolean.TryParse(value, out parsedValue);
+                if (parsedValue)
+                {
+                    result = true;
+                    break;
+                }
+            }
+            if (parsedAnyBool) { return result; }
+            return null;
+        }
+
+        private static RouteValueDictionary getRouteValues(NameValueCollection parameters)
+        {
+            var routeValues = new RouteValueDictionary();
+
+            foreach (string key in parameters.Keys)
+            {
+                var value = DetermineBooleanParameterValue(parameters, key);
+                if (value.HasValue)
+                {
+                    routeValues.Add(key, value.Value);
+                }
+                else
+                {
+                    routeValues.Add(key, parameters[key]);
+                }
+
+            }
+            return routeValues;
+        }
+    }
+}
