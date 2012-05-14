df5c70c60485c34390988afecddc29ae1bda8374
Piotr Bober <pbober@power.com.pl>
2012-05-14 13:47:58 +0200

    Data.ByteSize: fix handling corner case for size==0 in method SizeFormatted

--- sources/PowerMedia.Common/Data/ByteSize.cs
+++ sources/PowerMedia.Common/Data/ByteSize.cs
@@ -9,8 +9,8 @@ namespace PowerMedia.Common.Data
     {
         public static string SizeFormatted(ulong size)
         {
+            if (size == 0) { return "0B"; }
 
-            var SizeInBytes = size;
             string[] suf = { "B", "KB", "MB", "GB", "TB", "PB" };
             int place = Convert.ToInt32(Math.Floor(Math.Log(size, 1024)));
             double num = Math.Round(size / Math.Pow(1024, place), 1);
